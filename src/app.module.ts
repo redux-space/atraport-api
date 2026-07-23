4193864982eb3db3e325518ab3ee9d4bd4734bf3
nafsonig <saurinkudelivery@gmail.com>
2026-07-23 14:33:15 +0000

    implemented the fee rebalancer

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { PortfolioModule } from './portfolio/portfolio.module';
 import { RiskModule } from './risk/risk.module';
 import { ContractsModule } from './contracts/contracts.module';
+import { RebalancingModule } from './rebalancing/rebalancing.module';
 
 @Module({
-  imports: [AuthModule, PortfolioModule, RiskModule, ContractsModule],
+  imports: [AuthModule, PortfolioModule, RiskModule, ContractsModule, RebalancingModule],
   controllers: [AppController],
   providers: [AppService]
 })
