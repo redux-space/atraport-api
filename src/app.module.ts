31915c7d5504e09635cdb089161fffa07a09e751
codesailor4 <codesailor4@gmail.com>
2026-07-24 11:25:21 +0100

    implemented the unstaking

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,32 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PortfolioModule } from './portfolio/portfolio.module';
 import { RiskModule } from './risk/risk.module';
 import { ContractsModule } from './contracts/contracts.module';
+import { StakingModule } from './staking/staking.module';
+import { EmergencyUnstake } from './staking/entities/emergency-unstake.entity';
 
 @Module({
-  imports: [AuthModule, PortfolioModule, RiskModule, ContractsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'astraport',
+      entities: [EmergencyUnstake],
+      synchronize: true,
+    }),
+    AuthModule, 
+    PortfolioModule, 
+    RiskModule, 
+    ContractsModule,
+    StakingModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
